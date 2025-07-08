0dfce279acdf75842e7c98ba950fcca779e21b2f
ItishreeNayak75 <nayakitishree758@gmail.com>
2025-07-08 12:05:29 +0530

    uploading INTERFACE

---
+++ OOPS/INTERFACE/Playable.java
@@ -0,0 +1,20 @@
+interface Playable {
+    void play();
+}
+class Football implements Playable {
+   			 public void play() {
+        		 System.out.println("Playing Football!");
+    			}
+		}
+class Volleyball implements Playable {
+   			 public void play() {
+        		 System.out.println("Playing Volleyball!");
+    			}
+		}
+
+class Basketball implements Playable {
+    			public void play() {
+        		System.out.println("Playing Basketball!");
+    			}
+		}
+
